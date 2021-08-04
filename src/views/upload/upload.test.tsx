b0b5ecb83c494158989a3bac7f0411fbdc6085c3
SampsonM <SampsonM@github.com>
2021-08-04 13:21:30 +0100

    Moved upload logic into useEffect hook

--- src/views/upload/upload.test.tsx
+++ src/views/upload/upload.test.tsx
@@ -1,47 +1,87 @@
 import React from 'react'
-import { render, waitFor, fireEvent } from '@testing-library/react'
+import { render, waitFor, fireEvent, findByTestId, Query } from '@testing-library/react'
 import Upload from './upload'
 import axios from 'axios'
+import { MemoryRouter } from 'react-router-dom'
 
 const mockAxios = axios as jest.Mocked<typeof axios>
+const mockHistoryPush = jest.fn()
+
+jest.mock('react-router-dom', () => ({
+  ...jest.requireActual('react-router-dom'),
+  useHistory: () => ({
+    push: mockHistoryPush,
+  }),
+}));
 
 describe('Upload', () => {
-  it('should upload cat image to api', async () => {
-    mockAxios.post.mockResolvedValueOnce({
-      data: {},
-    })
+  afterEach(() => {
+    jest.clearAllMocks()
+  })
 
-    const file = new File(['test-image'], 'test-image.png', {
-      type: 'image/png',
-    })
+  describe('when an image is successfully uploaded', () => {
+    beforeEach(async () => {
+      mockAxios.post.mockResolvedValueOnce({ data: {} })
 
-    const { getByTestId } = render(<Upload />)
-    const uploader = getByTestId('cat-file-uploader')
+      const file = new File(['test-image'], 'test-image.png', {
+        type: 'image/png',
+      })
 
-    await waitFor(() =>
-      fireEvent.change(uploader, {
+      const { getByTestId } = render(<MemoryRouter><Upload /></MemoryRouter>)
+
+      fireEvent.change(getByTestId('cat-file-uploader'), {
         target: { files: [file] },
-      }),
-    )
+      })
 
-    fireEvent.submit(getByTestId('cat-upload-form'))
+      fireEvent.submit(getByTestId('cat-upload-form'))
+    })
 
-    expect(
-      mockAxios.post,
-    ).toHaveBeenCalledWith(
-      'https://api.thecatapi.com/v1/images/upload',
-      expect.any(FormData),
-      {
-        headers: {
-          'Content-Type': 'multipart/form-data',
-          'x-api-key': 'a9c159b6-bbab-4527-88c9-b01675355642',
+    it('should upload cat image to api', () => {
+      expect(
+        mockAxios.post,
+      ).toHaveBeenCalledWith(
+        'https://api.thecatapi.com/v1/images/upload',
+        expect.any(FormData),
+        {
+          headers: {
+            'Content-Type': 'multipart/form-data',
+            'x-api-key': 'a9c159b6-bbab-4527-88c9-b01675355642',
+          },
         },
-      },
-    )
+      )
 
-    // Depending on the level of testing the team feels relevant for the uploading of an
-    // image depends on how this part is tested
-    // I think the image being successfully uploaded is more suited to an E2E test
-    // so have decided against testing the form data structure here
+      // Depending on the level of testing the team feels relevant for the uploading of an
+      // image depends on how this part is tested
+      // I think the image being successfully uploaded is more suited to an E2E test
+      // so have decided against testing the form data structure here
+    })
+
+    it('should redirect user to home page', () => {
+      expect(mockHistoryPush).toHaveBeenCalledWith('/')
+    })
+  })
+
+  describe('when an image is NOT successfully uploaded', () => {
+    let getByTestId: any
+    let findByTestId: any
+
+    beforeEach(() => {
+      mockAxios.post.mockRejectedValueOnce(new Error(''))
+
+      const app = render(<MemoryRouter><Upload /></MemoryRouter>)
+      getByTestId = app.getByTestId
+      findByTestId = app.findByTestId
+
+      fireEvent.change(getByTestId('cat-file-uploader'), {
+        target: { files: [] },
+      })
+
+      fireEvent.submit(getByTestId('cat-upload-form'))
+    })
+
+    it('should display errors returned form API', async () => {
+      const error = await findByTestId('cat-upload-form-error')
+      expect(error).toBeDefined()
+    })
   })
 })
--- src/views/upload/upload.tsx
+++ src/views/upload/upload.tsx
@@ -1,11 +1,17 @@
 import axios from 'axios'
-import React, { FormEvent, useState } from 'react'
+import React, { FormEvent, useState, useEffect, useRef } from 'react'
+import { act } from 'react-dom/test-utils'
+import { useHistory } from 'react-router-dom'
 import './upload.css'
 
-interface UploadProps {}
+interface UploadProps { }
 
 const Upload: React.FC<UploadProps> = () => {
-  const [imageFile, setImageFile] = useState<File>()
+  const [imageFile, setImageFile] = useState<string | Blob>()
+  const [submitting, setSubmitting] = useState(false)
+  const [error, setError] = useState(false)
+  const isMounted = useRef(false);
+  const history = useHistory()
 
   const handleImageSelect = (event: FormEvent<HTMLInputElement>) => {
     const target = event.target as HTMLInputElement
@@ -14,45 +20,60 @@ const Upload: React.FC<UploadProps> = () => {
     setImageFile(image)
   }
 
-  const handleSubmit = async (event: FormEvent) => {
+  const handleSubmit = (event: FormEvent) => {
     event.preventDefault()
-
-    // TODO: could add debounce here
-
-    if (imageFile) {
-      try {
-        const formData = new FormData()
-        formData.append('file', imageFile)
-
-        await axios.post(
-          'https://api.thecatapi.com/v1/images/upload',
-          formData,
-          {
-            headers: {
-              'x-api-key': 'a9c159b6-bbab-4527-88c9-b01675355642',
-              'Content-Type': 'multipart/form-data',
-            },
-          },
-        )
-      } catch (err) {
-        // TODO: add err handling here
-      }
-    } else {
-      // TODO: add err handling here
-    }
+    setSubmitting(true)
   }
+
+  useEffect(() => {
+    async function postImage() {
+      if (imageFile) {
+        try {
+          const formData = new FormData()
+          formData.append('file', imageFile)
+
+          await axios.post(
+            'https://api.thecatapi.com/v1/images/upload',
+            formData,
+            {
+              headers: {
+                'x-api-key': 'a9c159b6-bbab-4527-88c9-b01675355642',
+                'Content-Type': 'multipart/form-data',
+              },
+            },
+          )
+
+          history.push('/')
+        } catch (err) {
+          act(() => setError(true))
+        }
+      } else {
+        act(() => setError(true))
+      }
+    }
+
+    if (isMounted.current) {
+      postImage();
+    } else {
+      isMounted.current = true
+    }
+  }, [submitting]);
 
   return (
     <div className="cat-upload">
-      <form data-testid="cat-upload-form" onSubmit={handleSubmit}>
-        <label>
+      <form data-testid="cat-upload-form" className="cat-upload__form" onSubmit={handleSubmit}>
+        <label className="cat-upload__file-label">
           Cat image:
           <input
+            className="cat-upload__file-input"
             data-testid="cat-file-uploader"
             type="file"
             onChange={handleImageSelect}
           />
         </label>
+
+        {error && <div data-testid="cat-upload-form-error">Error</div>}
+
         <input
           data-testid="cat-upload-submit-btn"
           className="cat-upload__submit"
